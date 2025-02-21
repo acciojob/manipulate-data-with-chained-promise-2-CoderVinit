694d39656b3e972e8297b9d108e617aa5749bd6a
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-02-21 10:03:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your JS code here. If required.
+function processArray(){
+	let outputDiv = document.getElementById("output");
+
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve([1,2,3,4])
+		},3000)
+	})
+	.then((arr)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				const filtereArr = arr.filter(num=>num%2===0)
+				outputDiv.textContent = filtereArr.join(", ");
+				resolve(filtereArr)
+			},1000)
+		})
+	})
+	.then((filteredArr)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				const multipliedArr = filteredArr.map(num=>num*2)
+				outputDiv.textContent = multipliedArr.join(", ")
+				resolve(multipliedArr)
+			},2000)
+		})
+	})
+}
+
+processArray()
+
